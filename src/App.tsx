2ffa50931df5a37f81950edf35c01c4dd6f036f3
Timofey Goncharov <gtosss@mail.ru>
2024-03-05 17:04:40 +0300

    1 module lesson 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Foo} from "./components/Foo";
+import React, {Suspense} from 'react';
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div className='app'>
-            qqqqqqqqqqqqqqqqq
-            <Foo/>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/'}>Main</Link>
+            <Suspense fallback={<div>Loading_____</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
