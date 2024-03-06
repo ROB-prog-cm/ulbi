743739a15f31b3edd87a392ac3b291dcdc152695
Timofey Goncharov <gtosss@mail.ru>
2024-03-06 15:29:35 +0300

    1 module lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, {Suspense} from 'react';
-import './index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
+import './styles/index.scss'
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/about'}>About</Link>
             <Link to={'/'}>Main</Link>
             <Suspense fallback={<div>Loading_____</div>}>
