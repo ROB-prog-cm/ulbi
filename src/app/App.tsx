fa0046d35fd97be3280dea268933f9faa2ef6c38
Timofey Goncharov <gtosss@mail.ru>
2024-03-21 09:24:14 +0300

    2 module lesson 12

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import {Link, Route, Routes} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import './styles/index.scss'
 import {cl} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
         <div className={cl('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/about'}>About</Link>
-            <Link to={'/'}>Main</Link>
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,9 +2,11 @@
 type Mods = Record<string, boolean | string>
 
 // classNames
-export function cl(cls: string, mods: Mods, additional: string[]): string {
-    return [cls, ...additional, ...Object.entries(mods)
-        .filter(([cl, value]) => Boolean(value))
+export function cl(cls: string, mods?: Mods, additional?: string[]): string {
+    return [
+        cls,
+        ...additional.filter(Boolean),
+        ...Object.entries(mods).filter(([cl, value]) => Boolean(value))
         .map(([cl]) => cl)]
         .join(' ')
 }
