961b997bd02301acc336288cdfc8f76fd70dc430
Timofey Goncharov <gtosss@mail.ru>
2024-03-29 00:55:05 +0300

    2 module lesson 20

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,11 @@
 type Mods = Record<string, boolean | string>
 
 // classNames
-export function cl(cls: string, mods?: Mods, additional?: string[]): string {
+export function cl(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
         ...additional.filter(Boolean),
-        ...Object.entries(mods).filter(([cl, value]) => Boolean(value))
-            .map(([cl]) => cl)]
-        .join(' ');
+        ...Object.entries(mods)
+            .filter(([cl, value]) => Boolean(value))
+            .map(([cl]) => cl)].join(' ');
 }
--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -2,7 +2,6 @@ import { cl } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import React from 'react';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
     className?: string
@@ -18,7 +17,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
     return (
         <div>
             <Button
-                className={cl(cls.LangSwitcher, {}, [className])}
+                className={cl('', {}, [className])}
                 theme={ThemeButton.CLEAR}
                 onClick={toggle}
             >
